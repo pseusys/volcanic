a31989f556fade8b30f8a4bbbf0f1c8b228243eb
pseusys <shveitsar215@gmail.com>
2023-04-14 16:31:21 +0200

    configuration done with file

--- sources/main.py
+++ sources/main.py
@@ -1,57 +1,40 @@
 #!/usr/bin/env python3
 from math import sqrt
-from typing import Callable
+from typing import Dict
 
-import numpy as np
 from perlin_noise import PerlinNoise
 
+from sources.config import read_config
+from sources.custom import terrain_generator
 from sources.objects import Terrain, Tree
 from sources.wrapper import Shader, Viewer
-from sources.utils import empty_grid, laplacian_of_gaussian, conditional_random_points
+from sources.utils import laplacian_of_gaussian, conditional_random_points
 
 
-def terrain_generator(carrier_func: Callable[[float, float], float], noise_func: Callable[[float, float], float], size_x: int, size_z: int, carrier_value: float = 5., noise_value: float = 1.):
-    carrier = empty_grid(x=size_x, z=size_z)
-    noise = empty_grid(x=size_x, z=size_z)
-
-    for x in range(-size_x // 2, size_x // 2):
-        for z in range(-size_z // 2, size_z // 2):
-            idx = (x + size_x // 2) * size_x + (z + size_z // 2)
-            carrier[idx] = carrier_func(x, z)
-            noise[idx] = noise_func(x / size_x, z / size_z)
-
-    height_matrix = carrier_value * np.array(carrier, dtype=np.float64) + noise_value * np.array(noise, dtype=np.float64)
-
-    def generator(xc: int, zc: int) -> float:
-        return height_matrix[xc * size_x + zc]
-
-    return generator
-
-
-# -------------- main program and scene setup --------------------------------
-def main():
-    """ create a window, add scene objects, then run rendering loop """
-    viewer = Viewer(distance=150)
+def main(configs: Dict[str, Dict[str, int]]):
+    viewer = Viewer(distance=configs["general"]["distance"])
     shader = Shader("shaders/phong.vert", "shaders/phong.frag")
 
-    noise = PerlinNoise(octaves=15, seed=1)
-    laplacian_sigma = 5
-    sigma_radius = 5
-    xpix, zpix = 100, 100
+    xpix = configs["general"]["size_x"]
+    zpix = configs["general"]["size_z"]
+
+    noise = PerlinNoise(octaves=configs["terrain"]["perlin_octaves"], seed=1)
+    laplacian_sigma = configs["terrain"]["laplacian_sigma"]
+    sigma_radius = configs["terrain"]["sigma_radius"]
 
     xav = xpix / 2
     zav = zpix / 2
-    tree_number = 50
-    tree_margin = 5
-    tree_height = 3
+    tree_number = configs["trees"]["tree_number"]
+    tree_margin = configs["trees"]["tree_margin"]
+    tree_height = configs["trees"]["tree_height"]
 
     generator = terrain_generator(
         lambda x, z: laplacian_of_gaussian(x, z, laplacian_sigma),
         lambda x, z: noise([x, z]),
         xpix,
         zpix,
-        50.,
-        1.
+        configs["terrain"]["carrier_weight"],
+        configs["terrain"]["perlin_weight"]
     )
     terrain = Terrain(shader, xpix, zpix, generator=generator)
     viewer.add(terrain)
@@ -63,9 +46,8 @@ def main():
     for tx, tz in trees:
         viewer.add(Tree(shader, tx, tz, terrain, tree_height))
 
-    # start rendering loop
     viewer.run()
 
 
 if __name__ == '__main__':
-    main()                     # main function keeps variables locally scoped
+    main(read_config())
