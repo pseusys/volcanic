fb7861492c0412f246353ce0773dc03b51289b43
pseusys <shveitsar215@gmail.com>
2023-04-16 19:50:45 +0200

    more colors on island

--- sources/objects/terrain.py
+++ sources/objects/terrain.py
@@ -1,10 +1,11 @@
-from typing import Callable, Optional
+from bisect import bisect_left
+from typing import Callable, Optional, Tuple
 
 import numpy as np
 import numpy.typing as npt
 
 from ..wrapper import Shader, Mesh
-from ..utils import triangle_normal, empty_grid
+from ..utils import triangle_normal, empty_grid, lerp
 
 
 def _flat_gen(_: int, __: int) -> float:
@@ -14,11 +15,9 @@ def _flat_gen(_: int, __: int) -> float:
 class Terrain(Mesh):
     """ Class for drawing a terrain object """
 
-    _MIDDLE_EDGE = 0.3
-    _LOWER_COLOR = (0, 0.666, 0)
-    _MIDDLE_COLOR = (0.333, 0.333, 0)
-    _UPPER_COLOR = (0.999, 0.999, 0.666)
-    _FLUCTUATIONS = 0.001
+    _EDGE_MAP = np.array([.0, .1, .2, .5, .9, 1.1], dtype=np.float64)
+    _COLOR_MAP = np.array([(.8, .8, .0), (1., 1., .4), (.0, .8, .0), (.2, .2, .0), (.6, .6, .4), (.8, .8, .8)], dtype=np.float64)
+    _SHINY_MAP = np.array([(64.,), (64.,), (32.,), (64.,), (8.,), (1.,)], dtype=np.float64)
 
     def __init__(self, shader: Shader, size_x: int, size_z: int, radius: int, generator: Optional[Callable[[int, int], float]] = None):
         if size_x <= 1 or size_z <= 1:
@@ -43,22 +42,7 @@ class Terrain(Mesh):
         self._normals = empty_grid(x=size_x, z=size_z, init=list)
         for x in range(size_x - 1):
             for z in range(size_z - 1):
-                vci = x * size_x + z  # Vertex current index
-                vbi = x * size_x + (z + 1)  # Vertex below index
-                vri = (x + 1) * size_x + z  # Vertex right index
-                voi = (x + 1) * size_x + (z + 1)  # Vertex opposite index
-
-                odd_triangle = (vci, vbi, voi)
-                even_triangle = (voi, vri, vci)
-                index += odd_triangle + even_triangle
-
-                odd_normal = triangle_normal(*[self._position[i] for i in odd_triangle])
-                even_normal = triangle_normal(*[self._position[i] for i in even_triangle])
-
-                for vertex in odd_triangle:
-                    self._normals[vertex] += [odd_normal]
-                for vertex in even_triangle:
-                    self._normals[vertex] += [even_normal]
+                index += self._compute_index_and_normals(x, size_x, z)
 
         # Calculate average normal for every vertex
 
@@ -68,24 +52,16 @@ class Terrain(Mesh):
 
         # Calculate lower and higher height maps
 
-        minimal = self._position.min(axis=0)[1]
-        maximal = self._position.max(axis=0)[1]
-        edge = (maximal - minimal) * self._MIDDLE_EDGE + minimal
-        lower_diff = np.array(self._MIDDLE_COLOR, dtype=np.float64) - np.array(self._LOWER_COLOR, dtype=np.float64)
-        upper_diff = np.array(self._UPPER_COLOR, dtype=np.float64) - np.array(self._MIDDLE_COLOR, dtype=np.float64)
+        self._minimal_height = np.amin(self._position, axis=0)[1]
+        self._maximal_height = np.amax(self._position, axis=0)[1]
+        self._height_difference = self._maximal_height - self._minimal_height
 
         # Calculate material color depending on height
 
         k_d = empty_grid(x=size_x, z=size_z)
-        s = empty_grid(x=size_x, z=size_z, init=lambda: (64.,))
+        s = empty_grid(x=size_x, z=size_z)
         for idx in range(len(self._position)):
-            if self._position[idx][1] <= edge:
-                height = (self._position[idx][1] - minimal) / (edge - minimal)
-                k_d[idx] = np.array(self._LOWER_COLOR, dtype=np.float64) + lower_diff * height
-            else:
-                height = (self._position[idx][1] - edge) / (maximal - edge)
-                k_d[idx] = np.array(self._MIDDLE_COLOR, dtype=np.float64) + upper_diff * height
-                s[idx] = np.array((64.,), dtype=np.float64) * (1 - height)
+            k_d[idx], s[idx] = self._get_edge_index_fraction(self._position[idx][1])
 
         k_a = np.array(empty_grid(x=size_x, z=size_z, init=lambda: (0, 0, 0)), dtype=np.float64)
         k_s = np.array(empty_grid(x=size_x, z=size_z, init=lambda: (1, 1, 1)), dtype=np.float64)
@@ -95,6 +71,33 @@ class Terrain(Mesh):
         colors = dict(k_a=k_a, k_d=np.array(k_d, dtype=np.float64), k_s=k_s, s=np.array(s, dtype=np.float64))
         attributes = dict(position=self._position, normal=self._normals, **colors)
         super().__init__(shader, index=index, attributes=attributes)
+
+    def _compute_index_and_normals(self, x, size_x, z) -> Tuple[float, float, float, float, float, float]:
+        vci = x * size_x + z  # Vertex current index
+        vbi = x * size_x + (z + 1)  # Vertex below index
+        vri = (x + 1) * size_x + z  # Vertex right index
+        voi = (x + 1) * size_x + (z + 1)  # Vertex opposite index
+
+        odd_triangle = (vci, vbi, voi)
+        even_triangle = (voi, vri, vci)
+        index = odd_triangle + even_triangle
+
+        odd_normal = triangle_normal(*[self._position[i] for i in odd_triangle])
+        even_normal = triangle_normal(*[self._position[i] for i in even_triangle])
+
+        for vertex in odd_triangle:
+            self._normals[vertex] += [odd_normal]
+        for vertex in even_triangle:
+            self._normals[vertex] += [even_normal]
+        return index
+
+    def _get_edge_index_fraction(self, height: float) -> Tuple[npt.NDArray[np.float64], float]:
+        height = (height - self._minimal_height) / self._height_difference
+        index = bisect_left(self._EDGE_MAP, height)
+        fraction = (height - self._EDGE_MAP[index - 1]) / (self._EDGE_MAP[index] - self._EDGE_MAP[index - 1])
+        color = lerp(self._COLOR_MAP[index - 1], self._COLOR_MAP[index], fraction)
+        shininess = lerp(self._SHINY_MAP[index - 1], self._SHINY_MAP[index], fraction)
+        return color, shininess
 
     def get_normal(self, x: int, z: int) -> npt.NDArray[np.float64]:
         return self._normals[x * self._x + z]
