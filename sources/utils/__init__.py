eb91d913ac160f8ccf737d25f5b11744ae8f4a93
pseusys <shveitsar215@gmail.com>
2023-04-21 12:58:53 +0200

    cleaned

--- sources/utils/__init__.py
+++ sources/utils/__init__.py
@@ -1,7 +1,7 @@
-from .transform import identity, lerp, sincos,quaternion, quaternion_from_euler, quaternion_slerp, quaternion_matrix, translate, scale, rotate, rotate
-from .trackball import Trackball
-from .mathematical import normal_normal, triangle_normal, laplacian_of_gaussian, square_extended, straight_angle_rotor, normalize, find_normal_rotation
-from .misc import conditional_random_points, terrain_generator
-from .perlin import noise
-from .np_misc import cached, vectorized
-from .wrap_misc import MeshedNode
+from .transform import identity, lerp, sincos,quaternion, quaternion_from_euler, quaternion_slerp, quaternion_matrix, translate, scale, rotate, rotate  # noqa: E402
+from .trackball import Trackball  # noqa: E402
+from .mathematical import normal_normal, triangle_normal, laplacian_of_gaussian, square_extended, straight_angle_rotor, normalize, find_normal_rotation  # noqa: E402
+from .misc import conditional_random_points, terrain_generator  # noqa: E402
+from .perlin import noise  # noqa: E402
+from .np_misc import cached, vectorized  # noqa: E402
+from .wrap_misc import MeshedNode  # noqa: E402
